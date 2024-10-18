74cb1bf56c7a78d56b40c9dfb9266a8aec1cb476
PJ Fanning <pjfanning@users.noreply.github.com>
2024-10-18 09:58:39 +0100

    Update HelloService.scala

--- src/main/scala/com/example/pekko/hello/HelloService.scala
+++ src/main/scala/com/example/pekko/hello/HelloService.scala
@@ -1,15 +1,12 @@
 package com.example.pekko.hello
 
 import org.apache.pekko.http.scaladsl.server.{Directives, Route}
-import org.slf4j.LoggerFactory
 
 import scala.concurrent.ExecutionContext
 
 class HelloService()(implicit executionContext: ExecutionContext)
   extends Directives {
-
-  private val logger = LoggerFactory.getLogger(classOf[HelloService])
-
+  
   val route: Route = getHello
 
   def getHello: Route =
